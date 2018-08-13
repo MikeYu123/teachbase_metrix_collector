179ca62177ae4c1055a00e5d2479f3369a6f992e
Yurchenkov Mikhail <m.yurchenkov@maximatelecom.ru>
2018-08-13 18:31:13 +0300

    TMC initial commit

---
+++ lib/teachbase_metrix_collector.rb
@@ -0,0 +1,5 @@
+require "teachbase_metrix_collector/version"
+
+module TeachbaseMetrixCollector
+  # Your code goes here...
+end
---
+++ lib/teachbase_metrix_collector/version.rb
@@ -0,0 +1,3 @@
+module TeachbaseMetrixCollector
+  VERSION = "0.1.0"
+end
